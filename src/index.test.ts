80ffd9481986c23c5825905215bb2192da229980
Muhammad Rendra <mnrendra@users.noreply.github.com>
2024-06-19 20:01:37 +0800

    feat: initial feature

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@ import mainStub from '@tests/stubs/main'
 import index from '.'
 
 describe('Test the root `index`!', () => {
-  it('Should return \'Hello, World!\' when given an empty argument!', () => {
+  it('Should return \'Initial feature!\' when given an empty argument!', () => {
     const result = index()
     expect(result).toBe(mainStub())
   })
--- src/main.test.ts
+++ src/main.test.ts
@@ -3,7 +3,7 @@ import mainStub from '@tests/stubs/main'
 import main from './main'
 
 describe('Test the `main` feature!', () => {
-  it('Should return \'Hello, World!\' when given an empty argument!', () => {
+  it('Should return \'Initial feature!\' when given an empty argument!', () => {
     const result = main()
     expect(result).toBe(mainStub())
   })
